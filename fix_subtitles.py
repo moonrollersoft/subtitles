57ec3026284402b11d22965993f9cf3f144b6f0a
Rafa Paricio <moonrollersoft@gmail.com>
2023-04-03 20:45:47 +0200

    CLN: 79 chars

--- fix_subtitles.py
+++ fix_subtitles.py
@@ -5,11 +5,22 @@ from src.subtitles_fixer import SubtitlesFixer
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Fix subtitles, renaming the matched files.')
-    parser.add_argument('-p', '--path', dest='path', type=str, help='Path to fix subtitles', required=True)
+    parser = argparse.ArgumentParser(
+        description='Fix subtitles, renaming the matched files.'
+    )
+    parser.add_argument(
+        '-p', '--path', dest='path', type=str, help='Path to fix subtitles',
+        required=True
+    )
     group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument('-s', '--series', dest='series', help='Fix series subtitles', action='store_true')
-    group.add_argument('-m', '--movies', dest='movie', help='Fix movies subtitles', action='store_true')
+    group.add_argument(
+        '-s', '--series', dest='series', help='Fix series subtitles',
+        action='store_true'
+    )
+    group.add_argument(
+        '-m', '--movies', dest='movie', help='Fix movies subtitles',
+        action='store_true'
+    )
 
     args = parser.parse_args()
     current_path = args.path
